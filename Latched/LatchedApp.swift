35bb191caf0bd52476c835c3d35a2b75151c7da0
AndresGutierrez <c0dingw.dre@gmail.com>
2022-03-19 14:28:24 -0700

    Initial Commit

---
+++ Latched/LatchedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatchedApp.swift
+//  Latched
+//
+//  Created by Andres Gutierrez on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LatchedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
