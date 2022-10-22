6d36711bc972423b66a22db0b289b9e3098d93c7
AndresGutierrez <c0dingw.dre@gmail.com>
2022-10-22 13:31:09 -0700

    Added Deletion functionality. Finished main Project functions!

--- Latched/Views/DateView.swift
+++ Latched/Views/DateView.swift
@@ -12,64 +12,22 @@ import CoreData
 struct DateView: View {
     @Environment(\.managedObjectContext) private var moc
     @Environment(\.dismiss) var dismiss
-    @State private var todaysDate = Date.now
-    @State private var showPrevious = false
-    
     let days: FetchedResults<DayEntity>
     
     var body: some View {
         NavigationView {
             VStack{
                 if days.isEmpty {
-                    Spacer()
-                    Text("No Times Recorded Yet!")
-                        .font(.title3)
-                        .bold()
-                    Spacer()
+                    emptyList()
                 } else {
-                    List{
-                        Section{
-                            ForEach(days, id: \.self){ day in
-                                Text(day.wrappedDateInfo)
-                                    .font(.title2)
-                                    .fontWeight(.bold)
-                                
-                                ForEach(day.wrappedTimes, id: \.self){ time in
-                                        HStack {
-                                            Text(time.wrappedDuration) +
-                                            Text(time.wrappedDuration == "1" ? " Minute" : " Minutes")
-                                            Spacer()
-                                            Text("Finished at: \(time.wrappedTimeEnded)")
-                                        }
-//
-//                                    .onDelete(perform: deleteDay)
-                                }
-                                
-                            }
-//                            .onDelete(perform: vm.deleteDay)
-                        }
-                        
-                    }
-                    
-                    
+                    trackedTimes()
                 }
-                Button {
-                    dismiss()
-                } label: {
-                    Text("Back to Tracker")
-                        .foregroundColor(.white)
-                        .padding()
-                        .padding(.horizontal)
-                        .background(Color(hue: 0.328, saturation: 0.796, brightness: 0.408))
-                        .cornerRadius(30)
-                }
+                dismissButton()
                 .onAppear {
                     UITableView.appearance().backgroundColor = UIColor.clear
                     UITableViewCell.appearance().backgroundColor = UIColor.clear
                 }
             }
-            
-            
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
             .navigationTitle("Track Log")
@@ -79,7 +37,69 @@ struct DateView: View {
 
 
 extension DateView{
-    func deleteDay(index: IndexSet) {
-//        moc.delete(days[index])
+    
+    private func emptyList() -> some View {
+        VStack{
+            Spacer()
+            Text("No Times Recorded Yet!")
+                .font(.title3)
+                .bold()
+            Spacer()
+        }
+    }
+    
+    private func trackedTimes() -> some View {
+        List{
+            Section{
+                ForEach(days, id: \.self){ day in
+                    Text(day.wrappedDateInfo)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    ForEach(day.wrappedTimes, id: \.self){ time in
+                            HStack {
+                                Text(time.wrappedDuration) +
+                                Text(time.wrappedDuration == "1" ? " Minute" : " Minutes")
+                                Spacer()
+                                Text("Finished at: ").bold() +
+                                     Text(time.wrappedTimeEnded)
+                            }
+                    }
+                    .onDelete { index in
+                        deleteTime(indexs: index, for: day)
+                    }
+                }
+                .onDelete(perform: deleteDay)
+            }
+        }
+    }
+    
+    private func dismissButton() -> some View {
+        Button {
+            dismiss()
+        } label: {
+            Text("Back to Tracker")
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(Color(hue: 0.328, saturation: 0.796, brightness: 0.408))
+                .cornerRadius(30)
+        }
+    }
+    
+    func deleteDay(indexs: IndexSet) {
+        for index in indexs {
+            let day = days[index]
+            moc.delete(day)
+        }
+        try? moc.save()
+    }
+    
+    func deleteTime(indexs: IndexSet, for times: DayEntity) {
+        for index in indexs {
+            let time = times.wrappedTimes[index]
+            moc.delete(time)
+        }
+        try? moc.save()
     }
 }
